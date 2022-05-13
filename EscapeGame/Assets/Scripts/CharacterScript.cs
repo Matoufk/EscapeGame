2760c0eff1a5156def8a2966ce809d3c71d3ad26
Mathis <mathis35230@gmail.com>
2022-05-13 15:56:08 +0200

    Update CharacterScript.cs
    Smoothing the deplacement

--- EscapeGame/Assets/Scripts/CharacterScript.cs
+++ EscapeGame/Assets/Scripts/CharacterScript.cs
@@ -6,6 +6,7 @@ public class CharacterScript : MonoBehaviour
 {
     Rigidbody rb;
     public float speed;
+    public float smoothSpeed= 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,9 @@ public class CharacterScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = transform.forward * Input.GetAxis("Vertical") * speed + transform.right * Input.GetAxis("Horizontal") * speed;
+        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * speed + transform.right * Input.GetAxis("Horizontal") * speed;
+       
+        rb.velocity = Vector3.Lerp(rb.velocity, newVelocity, smoothSpeed);
         transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
     }
 }
